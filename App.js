8d97ee5dca4fb2b896e0d410a4b3f48be89000d8
severine legon <severine@multiply.ai>
2021-06-16 23:11:52 +0100

    chore: remove unused code

--- App.js
+++ App.js
@@ -5,13 +5,12 @@
  * @format
  * @flow strict-local
  */
-import 'react-native-gesture-handler';
-import React from 'react';
-import {StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import React from 'react';
+import 'react-native-gesture-handler';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {Landing, Login} from './src/screens';
-import {createStackNavigator} from '@react-navigation/stack';
 
 const App = () => {
   const Stack = createStackNavigator();
@@ -29,24 +28,5 @@ const App = () => {
     </SafeAreaProvider>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
